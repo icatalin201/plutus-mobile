c62255d992eacbf5cb7b6128d78045a049d3ac62
Catalin Matache <icatalin201@gmail.com>
2021-01-29 22:19:08 +0200

    material theming

--- app/src/main/java/com/finance/plutus/mobile/app/ui/MainActivity.kt
+++ app/src/main/java/com/finance/plutus/mobile/app/ui/MainActivity.kt
@@ -1,10 +1,6 @@
 package com.finance.plutus.mobile.app.ui
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuItem
-import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.NavController
@@ -13,7 +9,6 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.NavigationUI
 import com.finance.plutus.mobile.R
 import com.finance.plutus.mobile.databinding.ActivityMainBinding
-import com.finance.plutus.mobile.login.ui.LoginActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,19 +19,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil
             .setContentView(this, R.layout.activity_main)
+        setSupportActionBar(binding.mainToolbar)
         setupNavigation()
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return super.onCreateOptionsMenu(menu)
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == R.id.logout) {
-            openLogoutDialog()
-        }
-        return super.onOptionsItemSelected(item)
     }
 
     private fun setupNavigation() {
@@ -49,24 +33,11 @@ class MainActivity : AppCompatActivity() {
         ).build()
         navController = Navigation.findNavController(this, R.id.main_container)
         NavigationUI.setupWithNavController(binding.mainBottomNv, navController)
-    }
-
-    private fun openLogoutDialog() {
-        val dialog = AlertDialog.Builder(this)
-            .setTitle(R.string.logout)
-            .setMessage(R.string.logout_confirmation_message)
-            .setNegativeButton(R.string.cancel) { dialog, _ -> dialog.dismiss() }
-            .setPositiveButton(R.string.confirm) { dialog, _ ->
-                dialog.dismiss()
-                closeAppAndOpenLogin()
-            }
-        dialog.show()
-    }
-
-    private fun closeAppAndOpenLogin() {
-        val intent = Intent(this, LoginActivity::class.java)
-        startActivity(intent)
-        finish()
+        NavigationUI.setupWithNavController(
+            binding.mainToolbar,
+            navController,
+            appBarConfiguration
+        )
     }
 
 }
